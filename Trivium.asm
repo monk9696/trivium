7766e207f1c12b4d29573bd5feddb1af1706ea55
monk9696 <nkrems@gmail.com>
2018-11-20 21:22:45 -0500

    Updated to the current smaller byte system
    Updated to the 3 byte key with a state of 3 words totaling 12 bytes, Set up the automatic loading into the state array and storing the sections in s0,s1,s2
    
    Also added the bitwise shifting so all that needs to be done is ating the newlt calculated t1,t2,t3 to their respective locations, along with the computation for that and the loop

--- Trivium.asm
+++ Trivium.asm
@@ -1,28 +1,32 @@
 .data
-	state: .space 32
-	key: .space 10
-	IV: .space 10
+	state: .space 12
+	key: .space 3
+	IV: .space 3
 .text
 
 
 dataInitialization:
 	#index
+	addi $t0, $zero, 0
+	
+	#key and Iv intialization
 	addi $t1, $zero, 4
 	addi $t0, $zero, 0
 	addi $t5, $zero, 8
 	sb  $t5, IV($t0)	
 	sw $t1, state($t0)
 	sb $t1, key($t0)
+	sb $t5, IV($t0)
 	addi $t0, $t0, 1
+	addi $t1, $t1, 3
 	sb $t1, key($t0)
+	sb $t5, IV($t0)
 	addi $t0, $t0, 1
+	addi $t1, $t1, 3
 	sb $t1, key($t0)
+	sb $t5, IV($t0)
 	addi $t0, $t0, 1
-	sb $t1, key($t0)
-	addi $t0, $t0, 1
-	sb $t1, key($t0)
-	addi $t0, $t0, 1
-
+	addi $t1, $t1, 3
 	#jumping to the end of array
 #	addi $t0, $zero, 68
 #	sw $t1, state($t0)	
@@ -34,12 +38,12 @@ dataInitialization:
 		lb $t1, key($t0)
 		sb $t1, state($t0)
 		addi $t0, $t0, 1
-		beq $t0, 10, keyExit
+		beq $t0, 3, keyExit
 		j keyLoop	
 	
 	keyExit:
-	
-	addi $t0, $zero, 12
+	#load in the IV into the state
+	addi $t0, $zero, 4
 	addi $t2, $zero, 0
 	IVLoop:
 		lb $t1, IV($t2)
@@ -47,23 +51,29 @@ dataInitialization:
 		addi $t0, $t0, 1
 		addi $t2, $t2, 1
 		
-		beq $t2, 10, IVExit
+		beq $t2, 3, IVExit
 		j IVLoop	
 	
 	IVExit:
 	
+	#allocate the last byte
+	addi $t0, $zero, 11
+	addi $t1, $zero, 224
+	sb $t1, state($t0)	
+	addi $t0, $zero, 0
+	lw $s0, state($t0)
+	addi $t0, $t0, 4
+	lw $s1, state($t0)
+	addi $t0, $t0, 4
+	lw $s2, state($t0)	
 	
-	li $v0,1
-	addi $a0, $t1, 0
-	syscall
+	j mainContinuation
 
 	
 
+mainContinuation:
 
-
-mainContunation:
-
-
+	
 
 
 bitwiseOperations:
@@ -73,5 +83,35 @@ bitwiseOperations:
 
 BitwiseShift:
 
-
-
+	sll $s0, $s0, 1
+	sll $s1, $s1, 1
+	sll  $s2, $s2, 1
+	addi $t0, $zero, 0
+	sw $s0, state($t0)
+	addi $t0, $t0, 4
+	sw $s1, state($t0)
+	addi $t0, $t0, 4
+	sw $s2, state($t0)	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
